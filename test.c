a04831537aebee144860909365c36f70962f3346
Andrii Denysenko <denysenko.andrii01@gmail.com>
2023-11-25 16:20:39 +0100

    change

--- test.c
+++ test.c
@@ -1,26 +1,87 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
 
-double to_basic_degrees(double degrees) {
-  double rounded_degrees = round(degrees / 360.0) * 90.0;
 
-  // Ensure the value is one of the specified basic degrees
-  if (rounded_degrees == 0) {
-    rounded_degrees = 360.0;
-  }
 
-  return rounded_degrees;
+void game_field(const int rows, const int columns, char field[rows][columns]){
+    printf("\n");
+    int tmp = 0;
+    for(int j = 0; j < (rows+2); j++){
+        for(int i = 0; i < (columns*4+3); i++){
+            // vytlaci prvy stlpec (cisla)
+            if(i == 0){
+                if(j < rows){
+                    if(j+1 < 10){
+                        printf(" %d", j+1);
+                    } else {
+                        printf("%d", j+1);
+                    }
+                    continue;
+                } else {
+                    printf("%c%c", ' ', ' ');
+                    continue;
+                }
+            }
+            // vytlaci druhy stlpec (medzery)
+            if(i == 1){
+                printf("%c", ' ');
+                continue;
+            }
+            // medzerove stlpce
+            if((i%2) == 1){
+                if(j != rows){
+                    printf("%c", ' ');
+                    continue;
+                } else if (j == rows){
+                    if(tmp == 0){
+                        printf("%c", '\\');
+                        tmp = 1;
+                        continue;
+                    } else if(tmp == 1){
+                        printf("%c", '/');
+                        tmp = 0;
+                        continue;
+                    }
+                }
+            }
+            // palickove stlpce
+            if((i%4) == 2){
+                if(j < rows){
+                    printf("%c", '|');
+                    continue;
+                } else if(j == rows){
+                    printf("%c", '^');
+                    continue;
+                } else {
+                    printf("%c", ' ');
+                    continue;
+                }
+            }
+            // znakove stlpce
+            if((i%4) == 0){
+                if(j < rows){
+                    printf("%c", field[j][i/4-1]);
+                    continue;
+                } else if(j == rows){
+                    printf("%c", '_');
+                    continue;
+                } else if(j > rows){
+                    printf("%d", i/4);
+                    continue;
+                }
+            }
+        }
+        printf("\n");
+    }
 }
+int main()
+{
+  int rows = 4, columns = 4;
+  char field[rows][columns];
+  game_field(6, 4, field);
 
-int main() {
-  double input_degrees;
-
-  printf("Enter a value in degrees: ");
-  scanf("%lf", &input_degrees);
-
-  double basic_degrees = to_basic_degrees(input_degrees);
-
-  printf("%lf degrees is equal to %lf basic degrees.\n", input_degrees, basic_degrees);
-
-  return 0;
+  return 0; 
 }
--- test.py
+++ test.py
@@ -1,59 +1,50 @@
-MORSE_CODE_DICT = {
-    'A': '.-',
-    'B': '-...',
-    'C': '-.-.',
-    'D': '-..',
-    'E': '.',
-    'F': '..-.',
-    'G': '--.',
-    'H': '....',
-    'I': '..',
-    'J': '.---',
-    'K': '-.-',
-    'L': '.-..',
-    'M': '--',
-    'N': '-.',
-    'O': '---',
-    'P': '.--.',
-    'Q': '--.-',
-    'R': '.-.',
-    'S': '...',
-    'T': '-',
-    'U': '..-',
-    'V': '...-',
-    'W': '.--',
-    'X': '-..-',
-    'Y': '-.--',
-    'Z': '--..',
-    '1': '.----',
-    '2': '..---',
-    '3': '...--',
-    '4': '....-',
-    '5': '.....',
-    '6': '-....',
-    '7': '--...',
-    '8': '---..',
-    '9': '----.',
-    '0': '-----',
-    '.': '.-.-.-',
-    ',': '--..--',
-    '?': '..--..',
-    '/': '-..-.',
-    '-': '-....-',
-    '(': '-.-.--',
-    ')': '-.--.-',
-}
+import random
+# Define colors and their corresponding representation
+colors = {"RED": "R", "GREEN": "G", "BLUE": "B"}
+# Initialize the game state
+tubes = []
+for _ in range(5):
+    tubes.append([])
+# Create and distribute balls randomly
+balls = []
+for _ in range(20):
+    color = random.choice(list(colors))
+    balls.append(color)
+    random_tube = random.randint(0, 4)
+    tubes[random_tube].append(color)
+# Function to print the current game state
+def print_game_state():
+    for tube in tubes:
+        print(tube)
+# Game loop
+while True:
+    print("\nCurrent Game State:")
+    print_game_state()
+    # Get user input for selecting tubes
+    source_tube = int(input("Select source tube (0-4): "))
+    destination_tube = int(input("Select destination tube (0-4): "))
+    # Validate user input
+    if source_tube < 0 or source_tube > 4 or destination_tube < 0 or destination_tube > 4:
+        print("Invalid tube selection. Please enter a valid tube number (0-4).")
+        continue
+    # Check if the source tube is empty or the destination tube is full
+    if len(tubes[source_tube]) == 0:
+        print("Source tube is empty. Please select a non-empty tube.")
+        continue
+    if len(tubes[destination_tube]) == 4:
+        print("Destination tube is full. Please select a non-full tube.")
+        continue
+    # Move the ball from source to destination
+    ball_color = tubes[source_tube].pop()
+    tubes[destination_tube].append(ball_color)
+    print("Moved ball from tube", source_tube, "to tube", destination_tube)
 
-def decode_morse_code(morse_code):
-    morse_code_dict_reverse = {v: k for k, v in MORSE_CODE_DICT.items()}
-    result = ''
-    for code in morse_code.split(' '):
-        if code in morse_code_dict_reverse:
-            result += morse_code_dict_reverse[code]
-        else:
-            result += '?'
-    return result
-
-morse_code = '... --- ...'
-decoded_text = decode_morse_code(morse_code)
-print(decoded_text)
+    # Check if all balls are sorted correctly
+    is_sorted = True
+    for tube in tubes:
+        if len(tube) == 0 or len(set(tube)) != 1:
+            is_sorted = False
+            break
+    if is_sorted:
+        print("Congratulations! You have successfully sorted all balls.")
+        break
