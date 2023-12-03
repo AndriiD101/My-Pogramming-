2cb1c926343a9bc140bd6b7e159dc3836d512fbc
Andrii Denysenko <denysenko.andrii01@gmail.com>
2023-12-03 17:45:42 +0100

    add folder ps5

--- ps5/qr.c
+++ ps5/qr.c
@@ -1,24 +1,123 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 #include <stdbool.h>
 
 void encode_char(const char character, bool bits[8])
 {
-
+    int letter_ascii = character;
+    for(int div = 7; div >= 0; div--)
+    {
+        if(letter_ascii%2 == 0){
+            bits[div] = letter_ascii%2;
+            letter_ascii/=2;
+        }
+        else {
+            bits[div] = letter_ascii%2;
+            letter_ascii/=2;
+        }
+    }
 }
+
+char decode_byte(const bool bits[8])
+{
+    int result_ascii=0;
+    int mid;
+    for(int i=8; i>0; i--)
+    {
+        mid = pow(2, 8-i) * bits[i-1];
+        result_ascii += mid;
+    }
+    return result_ascii;
+}
+
+void encode_string(const char string[], bool bytes[strlen(string)+1][8])
+{
+    char string_element;
+    int letter_ascii;
+    for(int elm = 0; elm < strlen(string); elm++)
+    {
+        string_element = string[elm];
+        int letter_ascii = string_element;
+        for(int div = 7; div >= 0; div--)
+        {
+        if(letter_ascii%2 == 0){
+            bytes[elm][div] = letter_ascii%2;
+            letter_ascii/=2;
+        }
+        else {
+            bytes[elm][div] = letter_ascii%2;
+            letter_ascii/=2;
+        }
+        }
+    }
+    for(int i = 0; i < 8; i++)
+    {
+        bytes[strlen(string)][i] = 0;
+    }
+}
+
+
 
 int main()
 {
     bool bits1[8];
-    encode_char('A', bits1);
+    encode_char('B', bits1);
     for(int i = 0; i < 8; i++){
     printf("%d", bits1[i]);
     }
     printf("\n");
     // prints: 01000001
 
-    bool bits2[8] = {0,1,0,0,0,0,0,1};
+    bool bits2[8] = {0,1,0,0,0,0,1,0};
     printf("%c\n", decode_byte(bits2));
     // prints: A
+    char* text = "Hello, how are you?";
+    const int len = strlen(text);
+    bool bytes1[len+1][8];
+    encode_string(text, bytes1);
+    for(int j = 0; j <= len; j++){
+    printf("%c: ", text[j]);
+    for(int i = 0; i < 8; i++){
+        printf("%d", bytes1[j][i]);
+    }
+    printf("\n");
+    }
+    // prints:
+    // H: 01001000
+    // e: 01100101
+    // l: 01101100
+    // l: 01101100
+    // o: 01101111
+    // ,: 00101100
+    //  : 00100000
+    // h: 01101000
+    // o: 01101111
+    // w: 01110111
+    //  : 00100000
+    // a: 01100001
+    // r: 01110010
+    // e: 01100101
+    //  : 00100000
+    // y: 01111001
+    // o: 01101111
+    // u: 01110101
+    // ?: 00111111
+    // : 00000000
+
+    bool bytes2[7][8] = {
+    {0,1,0,0,1,0,0,0},
+    {0,1,1,0,0,1,0,1},
+    {0,1,1,0,1,1,0,0},
+    {0,1,1,0,1,1,0,0},
+    {0,1,1,0,1,1,1,1},
+    {0,0,1,0,0,0,0,1},
+    {0,0,0,0,0,0,0,0}
+};
+char string[7];
+decode_bytes(7, bytes2, string);
+printf("%s\n", string);
+// prints: Hello!
+    return 0;
 }
--- test.c
+++ test.c
@@ -1,66 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
-int check_diagonal(int rows, int cols, const char board[rows][cols], int row, int col, char player_piece) {
-    //left-down to right-upper
-    int l_row = row - 1;
-    int l_col = col - 1;
-
-    while (l_row >= 0 && l_col >= 0 && board[l_row][l_col] == player_piece) {
-        l_row--;
-        l_col--;
-    }
-
-    int count = 0;
-    // Diagonal counting from right-upper to left-down
-    for (int r = l_row + 1, c = l_col + 1; r < rows && c < cols; r++, c++) {
-        if (board[r][c] != player_piece) {
-            break;
-        }
-        count++;
-    }
-
-    if (count >= 4) {
-        return 1;
-    }
-
-    //left-uppper to right-down
-    l_row = row + 1;
-    l_col = col - 1;
-
-    while (l_row < rows && l_col >= 0 && board[l_row][l_col] == player_piece) {
-        l_row++;
-        l_col--;
-    }
-
-    count = 0;
-    // Diagonal counting from left-upper to right-down
-    for (int r = l_row - 1, c = l_col + 1; r >= 0 && c < cols; r--, c++) {
-        if (board[r][c] != player_piece) {
-            break;
-        }
-        count++;
-    }
-
-    if (count >= 4) {
-        return 1;
-    }
-
-    return 0;
-}
-
-int main() {
-    int rows = 4;
-    int cols = 7;
-    char board[4][7] = {
-        {'X', '.', '.', '.', '.', '.', '.'},
-        {'O', 'X', '.', '.', '.', '.', '.'},
-        {'O', 'O', 'X', '.', '.', '.', '.'},
-        {'O', 'O', 'O', 'X', '.', '.', '.'}
-    };
-
-    int result = check_diagonal(rows, cols, board, 0, 0, 'X'); // Check diagonals around the last move
-
-    printf("Result: %d\n", result);
-
+int main()
+{
+    int x = pow(2, 0);
+    int ascii = 66;
+    char ch = ascii;
+    printf("ascii %d\n", x);
     return 0;
 }
