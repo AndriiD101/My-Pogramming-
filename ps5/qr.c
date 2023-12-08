05f6c9b36d46c6852313c51a490af172d637d2f5
Andrii Denysenko <denysenko.andrii01@gmail.com>
2023-12-08 14:54:32 +0100

    change ps5, add round 3, somethig changed in test

--- ps5/qr.c
+++ ps5/qr.c
@@ -39,7 +39,7 @@ void encode_string(const char string[], bool bytes[strlen(string)+1][8])
     for(int elm = 0; elm < strlen(string); elm++)
     {
         string_element = string[elm];
-        int letter_ascii = string_element;
+        letter_ascii = string_element;
         for(int div = 7; div >= 0; div--)
         {
         if(letter_ascii%2 == 0){
@@ -76,7 +76,32 @@ void decode_bytes(const int rows, bool bytes[rows][8], char string[rows])
     }
 }
 
+void bytes_to_blocks(const int cols, const int offset, bool blocks[offset * 8][cols], const int rows, bool bytes[rows][8]) {
+    for (int row = 0; row < offset; row++) {
+        for (int col = 0; col < cols; col++) {
+            for (int bit = 0; bit < 8; bit++) {
+                if (row * cols + col < rows) {
+                    blocks[row*8+bit][col] = bytes[row * cols + col][bit];
+                } else {
+                    blocks[row*8+bit][col] = 0;
+                }
+            }
+        }
+    }
+}
 
+
+void blocks_to_bytes(const int cols, const int offset, bool blocks[offset*8][cols], const int rows, bool bytes[rows][8]) {
+    for (int first=0; first < offset; first++){
+        int second=0;
+        for (int second=0; second < cols; second++){
+            for (int third=0; third< 8; third++) {
+                if (first*cols+second > rows) return;
+                bytes[first*cols+second][third] = blocks[first*8+third][second];
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -132,10 +157,62 @@ int main()
     {0,1,1,0,1,1,1,1},
     {0,0,1,0,0,0,0,1},
     {0,0,0,0,0,0,0,0}
-};
-char string[7];
-decode_bytes(7, bytes2, string);
-printf("%s\n", string);
-// prints: Hello!
+    };
+    char string[7];
+    decode_bytes(7, bytes2, string);
+    printf("%s\n", string);
+    // prints: Hello!
+    int length = 4+1, cols = 3, offset = 2;
+    bool bytes4[4+1][8] = {
+        {0,1,0,0,0,0,0,1},
+        {0,1,1,0,1,0,0,0},
+        {0,1,1,0,1,1,1,1},
+        {0,1,1,0,1,0,1,0},
+        {0,0,0,0,0,0,0,0}
+    };
+    bool blocks1[offset*8][cols];
+    bytes_to_blocks(cols, offset, blocks1, length, bytes4);
+    for(int j = 0; j < offset*8; j++){
+        for(int i = 0; i < cols; i++){
+        printf("%d ", (blocks1[j][i] == true) ? 1 : 0);
+    }
+    printf("\n");
+    if(j % 8 == 7){
+        printf("\n");
+    }
+    }
+    bool blocks2[2*8][3] = {
+    {0,0,0},
+    {1,1,1},
+    {0,1,1},
+    {0,0,0},
+    {0,1,1},
+    {0,0,1},
+    {0,0,1},
+    {1,0,1},
+    {0,0,0},
+    {1,0,0},
+    {1,0,0},
+    {0,0,0},
+    {1,0,0},
+    {0,0,0},
+    {1,0,0},
+    {0,0,0}
+    };
+    bool bytes9[length][8];
+    blocks_to_bytes(3, 2, blocks2, length, bytes9);
+    for(int j = 0; j < length; j++){
+        for(int i = 0; i < 8; i++){
+            printf("%d", bytes9[j][i]);
+        }
+        printf("\n");
+    }
+// prints:
+// 01000001
+// 01101000
+// 01101111
+// 01101010
+// 00000000
     return 0;
+    
 }
--- test.c
+++ test.c
@@ -1,13 +1,27 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
+void bytes_to_blocks(const int cols, const int offset, bool blocks[offset * 8][cols], const int rows, bool bytes[rows][8]) {
+    for (int row = 0; row < offset; ++row) {
+        for (int col = 0; col < cols; ++col) {
+            for (int bit = 0; bit < 8; ++bit) {
+                if (row * cols + col < rows) {
+                    blocks[row*8+bit][col] = bytes[row * cols + col][bit];
+                } else {
+                    blocks[row*8+bit][col] = 0;
+                }
+            }
+        }
+    }
+}
 
-int main()
-{
-    int x = pow(2, 0);
-    int ascii = 66;
-    char ch = ascii;
-    printf("ascii %d\n", x);
-    return 0;
+void bytes_to_blocks(const int cols, const int offset, bool blocks[offset*8][cols], const int rows, bool bytes[rows][8]) {
+    size_t first=0;
+    for (; first < offset; first++){
+        size_t second=0;
+        for (; second < cols; second++){
+            size_t third=0;
+            for (; third < 8; third++) {
+                if(first*cols+second < rows) blocks[first*8+third][second] =  bytes[first*cols+second][third];
+                else blocks[first*8+third][second] = 0;
+            }
+        }
+    }
 }
