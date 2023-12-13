034f06b572d1e9d875ce76f3f63b8e9d7a206339
Andrii Denysenko <denysenko.andrii01@gmail.com>
2023-12-13 15:26:08 +0100

    do ps7

--- TUKE/ps7/count_words.c
+++ TUKE/ps7/count_words.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TARGET_WORD_LOWER "ananas"
-#define TARGET_WORD_UPPER "ANANAS"
+#define TARGET_WORD_LOWER_B "bananas"
+#define TARGET_WORD_UPPER_B "BANANAS"
+#define TARGET_WORD_LOWER_A "ananas"
+#define TARGET_WORD_UPPER_A "ANANAS"
 #define TARGET_LENGTH 6
+
+#include <stdio.h>
+
+void writeCountToFile(FILE *file, int count) {
+    if (count < 10) {
+        fputc(count + '0', file);
+    } else {
+        int digits[10];
+        int temp = count;
+        int digit, i = 0;
+
+        while (temp > 0) {
+            digit = temp % 10;
+            digits[i++] = digit;
+            temp /= 10;
+        }
+        
+        for (i = i - 1; i >= 0; i--) {
+            fputc(digits[i] + '0', file);
+        }
+    }
+}
+
+
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -21,7 +47,7 @@ int main(int argc, char *argv[]) {
     int symbol = 0;
     char element;
     while ((element = fgetc(file)) != EOF) {
-        if (element == TARGET_WORD_LOWER[symbol] || element == TARGET_WORD_UPPER[symbol]) {
+        if (element == TARGET_WORD_LOWER_A[symbol] || element == TARGET_WORD_UPPER_A[symbol] || element == TARGET_WORD_LOWER_B[symbol] || element == TARGET_WORD_UPPER_B[symbol]) {
             symbol++;
         } else {
             symbol = 0;
@@ -34,7 +60,7 @@ int main(int argc, char *argv[]) {
     fclose(file);
 
     file = fopen(argv[1], "w");
-    fputc('0' + count, file);
+    writeCountToFile(file, count);
     fclose(file);
 
     return 0;
--- TUKE/ps7/every_second.c
+++ TUKE/ps7/every_second.c
@@ -1,144 +1,88 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int start(char pole[],int idx);
-//int medzera(char pole[],int idx);
-int stop(char pole[],int idx);
+int check_start(char array[], int index);
+int check_stop(char array[], int index);
 
-int main(int argc, char*argv[]){
+int main(int argc, char* argv[]) {
+    FILE* file = fopen(argv[1], "r");
 
-FILE*fp = fopen(argv[1],"r");
+    int file_size = 0;
+    char character;
+    while((character = fgetc(file)) != EOF) {
+        file_size++;
+    }
+    fclose(file);
 
-char znak = fgetc(fp);
-int size = 0;
+    char buffer[file_size];
 
-while(znak != EOF){
-size++;
-znak = fgetc(fp);
+    // Check start word
+    file = fopen(argv[1], "r");
+    int b_index = 0;
+    while((character = fgetc(file)) != EOF) {
+        buffer[b_index] = character;
+        if(check_start(buffer, b_index) == 1) {
+            break;
+        }
+        b_index++;
+    }
+
+    // Check stop word
+    char content[file_size];
+    int c_index = 0;
+    while((character = fgetc(file)) != EOF) {
+        content[c_index] = character;
+        if(check_stop(content, c_index) == 1) {
+            break;
+        }
+        c_index++;
+    }
+    content[c_index-4] = '\0';
+    fclose(file);
+
+    char result[file_size];
+    int r_index = 0;
+    int word_count = 0;
+    bool in_word = false;
+    int word_index = 0;
+
+    // Count words in content
+    for(int i = 0; i < c_index - 4; i++) {
+        if(content[i] == ' ') {
+            in_word = false;
+        } else if(!in_word) {
+            in_word = true;
+            word_count++;
+            word_index++;
+        }
+        if(word_index % 2 == 0) { 
+            result[r_index] = content[i];
+            r_index++;
+        }
+    }
+    result[r_index] = '\0';
+
+    FILE *outputFile = fopen(argv[2], "w");
+
+
+    for(int i = 0; i < r_index-1; i++) {
+        fputc(result[i], outputFile);
+    }
+    fclose(outputFile);
+
+    return 0;
 }
 
-fclose(fp);
-
-char buffer[size];
-
-FILE*fp2 = fopen(argv[1],"r");
-
-znak = fgetc(fp2);
-int k=0;
-while(znak != EOF){
-buffer[k] = znak;
-
-if(start(buffer,k) == 1){
-break;
-}
-
-znak = fgetc(fp2);
-k++;
-}
-
-char pole[size];
-
-    znak = fgetc(fp2);
-    int l=0;
-
-    while(znak != EOF){
-    pole[l] = znak;
-
-    if(stop(pole,l) == 1){
-    break;
-    }
-
-    znak = fgetc(fp2);
-    l++;
-    }
-    pole[l-5] = '\0';
-
-    buffer[size] = '\0';
-
-    fclose(fp2);
-
-    char result[size];
-    int idx = 0;
-
-    bool druhe = false;
-    bool first = true;
-
-    for(int i=0;i<l-5;i++){
-    if(pole[i] == ' '){
-    if(druhe == true){
-    druhe = false;
-    }
-    else{
-    druhe = true;
-    if(first == true){
-    i++;
-    first = false;
-    }
-    }
-    }
-
-    if(druhe == true){
-    result[idx] = pole[i];
-    idx++;
-    }
-    }
-    result[idx] = '\0';
-
-    FILE *fp3 = fopen(argv[2],"w");
-
-    for(int h=0;h<idx;h++){
-    fputc(result[h],fp3);
-    }
-
-    fclose(fp3);
-
-
-    return 0;
-    }
-
-    /* int medzera(char pole[],int idx){
-    while(pole[idx] != ' '){
-    idx++;
-    }
-    return idx;
-    } */
-
-    int start(char pole[],int idx){
-    if(pole[idx-5] != 'S' ){
-    return 0;
-    }
-    if(pole[idx-4] != 'T'){
-    return 0;
-    }
-    if(pole[idx-3] != 'A'){
-    return 0;
-    }
-    if(pole[idx-2] != 'R'){
-    return 0;
-    }
-    if(pole[idx-1] != 'T'){
-    return 0;
-    }
-    if(pole[idx] != ' ' ){
-    return 0;
+int check_start(char array[], int index) {
+    if(array[index-5] != 'S' || array[index-4] != 'T' || array[index-3] != 'A' || array[index-2] != 'R' || array[index-1] != 'T' || array[index] != ' ') {
+        return 0;
     }
     return 1;
-    }
+}
 
-
-    int stop(char pole[],int idx){
-    if(pole[idx-4] != 'S'){
-    return 0;
-    }
-    if(pole[idx-3] != 'T'){
-    return 0;
-    }
-    if(pole[idx-2] != 'O'){
-    return 0;
-    }
-    if(pole[idx-1] != 'P' ){
-    return 0;
+int check_stop(char array[], int index) {
+    if(array[index-4] != 'S' || array[index-3] != 'T' || array[index-2] != 'O' || array[index-1] != 'P') {
+        return 0;
     }
     return 1;
-
-    }
+}
