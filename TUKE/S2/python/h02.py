2cf2aff8ba2a5cb635667209a49a3ba15db2314d
Andrii Denysenko <denysenko.andrii01@gmail.com>
2024-07-27 14:04:41 +0200

    first commit from linux

--- TUKE/S2/python/h02.py
+++ TUKE/S2/python/h02.py
@@ -8,3 +8,4 @@ def deviders1(num: int = 0):
 #------------------------------------------------------------------------------
 def deviders2(num: int = 0):
     return [i for i in range(num + 1) if i % 3 == 0 and i % 5 == 0]
+
